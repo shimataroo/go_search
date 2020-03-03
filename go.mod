a4128cd777bc0a4586847b92877e9cdd8cadd388
shimataroo <shimataro0579@gmail.com>
2020-03-03 15:59:59 +0900

    update_to_indexer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shimataroo/go_search
 
 go 1.13
+
+require golang.org/x/tools v0.0.0-20200302225559-9b52d559c609 // indirect
--- indexer_test.go
+++ indexer_test.go
@@ -1,12 +1,51 @@
-package goserch
+package gosearch
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestUpdate(t *testing.T) {
 	collection := []string{
-		"Are you Shima ?",
-		"Helle World!",
+		"Are you Shima?",
+		"Hello World!",
 		"My name is Shima!",
 	}
+	indexer := NewIndexer(NewTokenizer())
 
+	for i, doc := range collection {
+		indexer.update(DocumentID(i), strings.NewReader(doc))
+	}
+
+	actual := indexer.index
+	expected := &Index{
+		Dictionary: map[string]PostingsList{
+			"are": NewPostingsList(
+				NewPosting(0, 0)),
+			"hello": NewPostingsList(
+				NewPosting(1, 0)),
+			"is": NewPostingsList(
+				NewPosting(2, 2)),
+			"my": NewPostingsList(
+				NewPosting(2, 0)),
+			"name": NewPostingsList(
+				NewPosting(2, 1)),
+			"shima": NewPostingsList(
+				NewPosting(0, 2),
+				NewPosting(2, 3)),
+			"world": NewPostingsList(
+				NewPosting(1, 1)),
+			"you": NewPostingsList(
+				NewPosting(0, 1)),
+		},
+		TotalDocsCount: 3,
+	}
+
+	fmt.Println(actual)
+	fmt.Println(expected)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Error("index error\n")
+	}
 }
